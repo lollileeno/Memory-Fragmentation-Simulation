e8e3494ca54e0cd0a8fbeb88542806e79d8c0a27
lollileeno <160266590+lollileeno@users.noreply.github.com>
2025-03-10 00:01:18 +0300

    Update MemoryManager.java

--- MemoryManager.java
+++ MemoryManager.java
@@ -16,15 +16,15 @@ public class MemoryManager {
 	   public void allocateMemory(String processID, int size, int strategy) {
 	        // Implement First-Fit, Best-Fit, Worst-Fit logic based on 'strategy' parameter
 		   switch(strategy) {
-		   case 1:  //first-fit
+		   case 1:  //first-fit"leena"
 			   
 			   break;
 			   
-		   case 2:   //Best-fit
+		   case 2:   //Best-fit"Almaha"
 			   
 			   break;
 			   
-		   case 3:   //Worst-fit
+		   case 3:   //Worst-fit"Shahad"
 			   
 			   break;
 			   
@@ -34,16 +34,16 @@ public class MemoryManager {
 		   }
 	    }
 
-	    public void deallocateMemory(String processID) {
+	    public void deallocateMemory(String processID) {//ghaida
 	        // Find the process and mark the block as free
 	    }
 	    
-	    public void printFirstMemoryStatus() {
+	    public void printFirstMemoryStatus() {//ghaida
 	        // Display block details
 	    }
 	
 
-	    public void printMemoryStatus() {
+	    public void printMemoryStatus() {//ghaida
 	        // Display block details
 	    }
 	
